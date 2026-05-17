3d2880ecd3557b90aa55c941b484c66dd16520fd
StockMonitorCH <stockmonitor@stock-monitor.ch>
2026-05-17 16:26:44 +0200

    Fix: APP_VERSION in stock-monitor.sh auf 5.3.2 korrigiert

--- stock-monitor.sh
+++ stock-monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Stock Monitor Launcher
-APP_VERSION="5.2.3"
+APP_VERSION="5.3.2"
 USER_LIB="${XDG_DATA_HOME:-$HOME/.local/share}/stock-monitor/lib"
 mkdir -p "$USER_LIB"
 export SM_LOG_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/stock-monitor"
